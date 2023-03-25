064592b98abc18dfcbc61ec649f0b26ef42c0801
VarshaaSK <varshaa.sk@m2pfintech.com>
2023-03-25 13:02:16 +0530

    Add. Icluded the Logic to Determine if the Tree is Mango tree or not

--- src/MangoTree.java
+++ src/MangoTree.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class MangoTree {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Tree Number : ");
+        int tree_number = scan.nextInt();
+
+        System.out.println("Enter the Number of Rows : ");
+        int row = scan.nextInt();
+
+        System.out.println("Enter the number of Coloumns : ");
+        int coloumn = scan.nextInt();
+
+        int[][] garden = new int[row][coloumn];
+        int treeCheck = 1;
+        int flag = 0;
+
+        for(int rowIteration = 0 ; rowIteration < garden.length; rowIteration++){
+            for(int coloumnIterator = 0 ; coloumnIterator < garden[rowIteration].length ;coloumnIterator++){
+                if(treeCheck == tree_number){
+
+                    if(rowIteration == 0 || coloumnIterator == 0 || coloumnIterator == garden[rowIteration].length - 1){
+                        System.out.println("yes");
+                        flag += 1;
+                        break;
+                    }
+                }
+                treeCheck += 1;
+            }
+            if(flag == 1){
+                break;
+            }
+
+        }
+        if(flag == 0){
+            System.out.println("no");
+        }
     }
 }
